2fe2aa4a3782f7e8a62b5bca37f0113005106bad
Filip Persson <filip.persson@me.com>
2013-11-19 20:37:16 +0100

    Added support for CORS

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-from bottle import route, run
+import bottle
+from bottle import response
 import browseGitHub
 import os
 
+class EnableCors(object):
+    name = 'enable_cors'
+    api = 2
+
+    def apply(self, fn, context):
+        def _enable_cors(*args, **kwargs):
+            # set CORS headers
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+            if bottle.request.method != 'OPTIONS':
+                # actual request; reply with the actual response
+                return fn(*args, **kwargs)
+
+        return _enable_cors
+
 print("Starting LappStore!")
-
 connection = browseGitHub.GitHubConnector()
-
 print("Loading data from GitHub...")
 connection.getAppsJSON()
-
 print("Server is ready!")
 
-@route('/')
+lappStore = bottle.app()
+
+@lappStore.route('/', method='GET')
 def getAllApps():
 	return connection.getAppsJSON()
 
-@route('/app/<app>' , method='GET')
+@lappStore.route('/app/<app>' , method='GET')
 def getApp(app = ""):
 	apps = connection.getAppsJSON()
 	if app in apps['apps']:
@@ -23,10 +40,11 @@ def getApp(app = ""):
 	else:
 		return {'error':'App does not exists'}
 
-@route('/refresh', method='POST')
+@lappStore.route('/refresh', method='POST')
 def refreshApps():
 	connection.getAppsJSON(True)
 	return {"status":"refresh successfull!"}
 
+lappStore.install(EnableCors())
 
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+lappStore.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
