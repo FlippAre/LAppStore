cdb5db79f41db94ba7d9b91c752afc2213ac788a
Filip Persson <filip.persson@me.com>
2013-12-03 19:57:01 +0100

    Change to handle new GitHub structure

--- browseGitHub.py
+++ browseGitHub.py
@@ -9,11 +9,12 @@ class GitHubConnector:
 	def __init__(self):
 		self.github_user = os.environ["GITHUB_USER"]
 		self.github_password = os.environ["GITHUB_PASSWORD"]
+		self.baseURL = "https://api.github.com/repos/Lundalogik/LimeBootstrapAppStore"
 		self.last_update = arrow.get( "1970-01-01T00:00:01Z" )
 		self.cached_return_json = {}
 
 	def getAppsJSON(self, forceRefresh = False):
-		repo = requests.get("https://api.github.com/repos/lundalogik/limebootstrap",auth=(self.github_user, self.github_password))
+		repo = requests.get(self.baseURL,auth=(self.github_user, self.github_password))
 		repo_json = json.loads(repo.text)
 		#Should new data be loaded or cache returned?
 
@@ -24,25 +25,26 @@ class GitHubConnector:
 			return_json["apps"] = []
 
 			#Find all apps
-			appDir = requests.get("https://api.github.com/repos/lundalogik/limebootstrap/contents/apps",auth=(self.github_user, self.github_password))
+			appDir = requests.get(self.baseURL + "/contents",auth=(self.github_user, self.github_password))
 			if appDir.ok:
 				appDir_json = json.loads(appDir.text)
 				j = 0
 				for i, item in enumerate(appDir_json):
 					#Check if it is an app, i.e a directory
 					if appDir_json[i]["type"] == "dir":
+						print("yes")
 						appname = appDir_json[i]["name"]
 						return_json["apps"].append({})
 						return_json["apps"][j].update({"name":appname})
 
 						#Load README for a app
-						readme = requests.get("https://api.github.com/repos/lundalogik/limebootstrap/contents/apps/"+appname+"/"+"README.md?ref=master",auth=(self.github_user, self.github_password))
+						readme = requests.get(self.baseURL+"/contents/"+appname+"/"+"README.md?ref=master",auth=(self.github_user, self.github_password))
 						if readme.ok:
 							json_readme = json.loads(readme.text)
 							return_json["apps"][j].update( {"readme":json_readme["content"]})
 
 						#Load app.json for a app
-						info = requests.get("https://api.github.com/repos/lundalogik/limebootstrap/contents/apps/"+appname+"/"+"app.json?ref=master",auth=(self.github_user, self.github_password), headers={"Accept":"application/vnd.github.VERSION.raw"})
+						info = requests.get(self.baseURL+"/contents/"+appname+"/"+"app.json?ref=master",auth=(self.github_user, self.github_password), headers={"Accept":"application/vnd.github.VERSION.raw"})
 						if info.ok:
 							json_info = json.loads(info.text)
 							return_json["apps"][j].update( {"info": json_info})
